a9679caac88cc041b6acead5b9dcb4258613e322
1Rafael117 <rafael_perez_in@comunidad.unam.mx>
2023-06-28 15:31:46 -0600

    proyecto base

---
+++ proyectoP/app/src/main/java/com/example/proyectop/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.proyectop
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 5000 // Duración del Splash Screen en milisegundos (5 segundos)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, SPLASH_TIME_OUT.toLong())
+    }
+}
